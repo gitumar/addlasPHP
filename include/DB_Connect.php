9fd7d3a0c00b191e7ab3776af730fbb369fde589
Umar <umarb8@yahoo.com>
2017-09-19 12:09:20 -0500

    Update DB_Connect.php

--- include/DB_Connect.php
+++ include/DB_Connect.php
@@ -14,7 +14,6 @@ class DB_Connect {
     // Connecting to database
     public function connect() {
         require_once 'Config.php';
-        // connecting to mysql
         //connecting to mysql
         $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD,DB_DATABASE);
         // Check connection
